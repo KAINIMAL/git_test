bf4fe9e05440035ca5fdadb304199253f239d13d
Kendrick <kendrickliu316@gmail.com>
2022-07-19 12:27:50 +0200

    first commit

---
+++ src/main/java/kendrick/GitTest.java
@@ -0,0 +1,11 @@
+package kendrick;
+
+/**
+ * @author Kendrick Liu
+ * @create 2022-07-12:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
